b572fc260161b40de7f3f9ef591da1d6e03145e0
chinh.dang <chinh.dang@ntq-solution.com.vn>
2020-12-02 18:25:32 +0700

    update

--- app/Modules/Member/Views/top1.blade.php
+++ app/Modules/Member/Views/top1.blade.php
@@ -34,6 +34,7 @@
             <div class="container">
                 <div class="row">
                     <input type="hidden" value="{{ url('api/v1/cart') }} " id="url-cart">
+                    <input type="hidden" value="{{ url('/review') }} " id="url-view">
                     <div class="col-12 col-xl-3">
                         <div class="deal-of-week_slide">
                             <div class="week-deal_top mini-tab-title underline pink">
