af8b717ef8bed4850db2f7a7dd48e25473587b28
Cường Lê Mạnh <cuonglvmphp@gmail.com>
2021-08-05 10:47:14 +0700

    test fb chat and email tempalte

--- app/Modules/Member/Views/emails/order/new_order.blade.php
+++ app/Modules/Member/Views/emails/order/new_order.blade.php
@@ -1,3 +1,9 @@
 @component('mail::message')
-test
+Xin chào {{$dataGuest['full_name']}}
+
+<p>Đơn hàng: {{$orderContent['name']}}</p>
+<p>Số lương: {{$orderContent['qty']}}</p>
+<p>Đã đặt hàng thành công.</p>
+<p>Chúng tôi sẽ sớm gửi hàng đến chỉ {{$dataGuest['address']}} trong thời gian sớm nhất.</p>
+<p>Cảm ơn bạn đã ủng hộ chúng tôi. Xin chân thành cảm ơn!!</p>
 @endcomponent
