560f0da107f876ba9277aba0172ac3abc521a96d
ChinhDH <chinhdh@ominext.com>
2021-08-07 19:00:35 +0700

    default

--- app/Modules/Admin/Views/includes/header.blade.php
+++ app/Modules/Admin/Views/includes/header.blade.php
@@ -1,31 +1,31 @@
 <header class="app-header navbar">
     <a class="logo-header-admin d-lg-none" href="{{route('admin.dashboard')}}">
-        <img src="/image/frontend/logo_footer.svg" alt="Oriental Club" class="logo-image">
+        <img src="/images/logo_hn_taphoa.png" alt="Ha Noi Tap Hoa" class="logo-image">
     </a>
     <button class="navbar-toggler sidebar-toggler d-lg-none" type="button" data-toggle="sidebar-show">
         <span class="navbar-toggler-icon"></span>
     </button>
 
     <a class="logo-header-admin d-md-down-none" href="{{route('admin.dashboard')}}">
-        <img src="/image/frontend/logo_footer.svg" alt="Oriental Club" class="logo-image">
+        <img src="/images/logo_hn_taphoa.png" alt="Ha Noi Tap Hoa" class="logo-image">
     </a>
     <button class="navbar-toggler sidebar-toggler d-md-down-none" type="button" data-toggle="sidebar-lg-show">
         <span class="navbar-toggler-icon"></span>
     </button>
 
-    <ul class="nav navbar-nav">
-        @if(config('locale.status') && count(config('locale.languages')) > 1)
-            <li class="nav-item px-3 dropdown">
-                <a class="nav-link dropdown-toggle nav-link" id="btn-language" data-toggle="dropdown" href="#" role="button"
-                   aria-haspopup="true" aria-expanded="false">
-                    <span class="" data-btn="btn-language">@lang('menus.language-picker.language')
-                        ({{ strtoupper(app()->getLocale()) }})</span>
-                </a>
+    {{--<ul class="nav navbar-nav">--}}
+        {{--@if(config('locale.status') && count(config('locale.languages')) > 1)--}}
+            {{--<li class="nav-item px-3 dropdown">--}}
+                {{--<a class="nav-link dropdown-toggle nav-link" id="btn-language" data-toggle="dropdown" href="#" role="button"--}}
+                   {{--aria-haspopup="true" aria-expanded="false">--}}
+                    {{--<span class="" data-btn="btn-language">@lang('menus.language-picker.language')--}}
+                        {{--({{ strtoupper(app()->getLocale()) }})</span>--}}
+                {{--</a>--}}
 
-                @include('includes.partials.lang')
-            </li>
-        @endif
-    </ul>
+                {{--@include('includes.partials.lang')--}}
+            {{--</li>--}}
+        {{--@endif--}}
+    {{--</ul>--}}
 
     <ul class="nav navbar-nav ml-auto">
         <li class="nav-item dropdown">
--- app/Modules/Admin/Views/includes/sidebar.blade.php
+++ app/Modules/Admin/Views/includes/sidebar.blade.php
@@ -13,127 +13,132 @@
                 </a>
             </li>
 
-            <li class="nav-item nav-dropdown {{
-                    active_class(Route::is('admin/auth*'), 'open')
-                }}">
-                <a class="nav-link nav-dropdown-toggle {{
-                        active_class(Route::is('admin/auth*'))
-                    }}" href="#">
-                    <i class="nav-icon far fa-user"></i>
-                    Quản lý sản phầm và danh mục
-                </a>
-                <ul class="nav-dropdown-items">
-                    <li class="nav-item">
-                        <a class="nav-link {{ active_class(\Request::is('admin/order*')) }}"
-                           href="{{ route('order.index') }}">
-                            <i class="nav-icon cil-envelope-closed"></i>
-                            @lang('labels.menu.order_management')
-                        </a>
-                    </li>
-
-                    <li class="nav-item">
-                        <a class="nav-link {{ active_class(\Request::is('admin/category*')) }}"
-                           href="{{ route('products.index') }}">
-                            <i class="nav-icon cil-envelope-closed"></i>
-                            @lang('labels.menu.product_management')
-                        </a>
-                    </li>
-
-                    <li class="nav-item">
-                        <a class="nav-link {{ active_class(\Request::is('admin/category*')) }}"
-                           href="{{ route('categories.index') }}">
-                            <i class="nav-icon cil-envelope-closed"></i>
-                            @lang('labels.menu.category_management')
-                        </a>
-                    </li>
-
-                    <li class="nav-item">
-                        <a class="nav-link {{ active_class(\Request::is('admin/units*')) }}"
-                           href="{{ route('units.index') }}">
-                            <i class="nav-icon cil-envelope-closed"></i>
-                            @lang('labels.menu.units_management')
-                        </a>
-                    </li>
-
-                    <li class="nav-item">
-                        <a class="nav-link {{ active_class(\Request::is('admin/tag*')) }}"
-                           href="{{ route('tags.index') }}">
-                            <i class="nav-icon cil-envelope-closed"></i>
-                            @lang('labels.menu.tag_management')
-                        </a>
-                    </li>
-
-                    <li class="nav-item">
-                        <a class="nav-link {{ active_class(\Request::is('admin/comment*')) }}"
-                           href="{{ route('comments.index') }}">
-                            <i class="nav-icon cil-envelope-closed"></i>
-                            @lang('labels.menu.comment_management')
-                        </a>
-                    </li>
-
-                    <li class="nav-item">
-                        <a class="nav-link {{ active_class(\Request::is('admin/shipping*')) }}"
-                           href="{{ route('shipping.index') }}">
-                            <i class="nav-icon cil-envelope-closed"></i>
-                            @lang('labels.menu.shipping_management')
-                        </a>
-                    </li>
-
-                </ul>
-            </li>
-
-            <li class="nav-item nav-dropdown {{
-                    active_class(Route::is('admin/auth*'), 'open')
-                }}">
-                <a class="nav-link nav-dropdown-toggle {{
-                        active_class(Route::is('admin/auth*'))
-                    }}" href="#">
-                    <i class="nav-icon far fa-user"></i>
-                    @lang('labels.menu.member_management')
-                </a>
-
-                <ul class="nav-dropdown-items">
-                    <li class="nav-item">
-                        <a class="nav-link nav-child {{
-                    active_class(\Request::is('admin/member*'))
-                }}" href="{{ route('member.index', 'female') }}">
-                            <i class="nav-icon cil-wc"></i>
-                            @lang('labels.menu.user_management')
-                        </a>
-                    </li>
-                    @can('balances_management')
-                        <li class="nav-item">
-                            <a class="nav-link nav-child {{
-                    active_class(Route::is('admin/balances'))
-                }}" href="{{ route('member.balances') }}">
-                                <i class="nav-icon cib-auth0"></i>
-                                @lang('labels.menu.balances')
-                            </a>
-                        </li>
-                    @endcan
-
-                    @can('offer_management')
-                        <li class="nav-item">
-                            <a class="nav-link nav-child {{
-                                active_class(Route::is('admin/auth/user*'))
-                            }}" href="{{ route('offers.index') }}">
-                                <i class="nav-icon cib-todoist"></i>
-                                @lang('labels.menu.offer')
-                            </a>
-                        </li>
-                    @endcan
-                </ul>
-            </li>
-
-            <li class="divider"></li>
             <li class="nav-item">
-                <a class="nav-link {{
-                    active_class(\Request::is('admin/contact*'))
-                }}" href="{{ route('contact.index') }}">
+                <a class="nav-link {{ active_class(\Request::is('admin/order*')) }}"
+                   href="{{ route('order.index') }}">
                     <i class="nav-icon cil-envelope-closed"></i>
-                    @lang('labels.menu.contact')
+                    @lang('labels.menu.order_management')
                 </a>
             </li>
+
+            <li class="nav-item">
+                <a class="nav-link {{ active_class(\Request::is('admin/category*')) }}"
+                   href="{{ route('products.index') }}">
+                    <i class="nav-icon cil-envelope-closed"></i>
+                    @lang('labels.menu.product_management')
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link {{ active_class(\Request::is('admin/category*')) }}"
+                   href="{{ route('categories.index') }}">
+                    <i class="nav-icon cil-envelope-closed"></i>
+                    @lang('labels.menu.category_management')
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link {{ active_class(\Request::is('admin/units*')) }}"
+                   href="{{ route('units.index') }}">
+                    <i class="nav-icon cil-envelope-closed"></i>
+                    @lang('labels.menu.units_management')
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link {{ active_class(\Request::is('admin/tag*')) }}"
+                   href="{{ route('tags.index') }}">
+                    <i class="nav-icon cil-envelope-closed"></i>
+                    @lang('labels.menu.tag_management')
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link {{ active_class(\Request::is('admin/comment*')) }}"
+                   href="{{ route('comments.index') }}">
+                    <i class="nav-icon cil-envelope-closed"></i>
+                    @lang('labels.menu.comment_management')
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link {{ active_class(\Request::is('admin/shipping*')) }}"
+                   href="{{ route('shipping.index') }}">
+                    <i class="nav-icon cil-envelope-closed"></i>
+                    @lang('labels.menu.shipping_management')
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link{{
+                                active_class(Route::is('admin/auth/user*'))
+                            }}" href="{{ route('banners.index') }}">
+                    <i class="nav-icon cil-inbox"></i>
+                    @lang('labels.menu.banner')
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link{{
+                                active_class(Route::is('admin/auth*'))
+                            }}" href="{{ route('news.index') }}">
+                    <i class="nav-icon cil-bell"></i>
+                    @lang('labels.menu.notification_management')
+                </a>
+            </li>
+
+            {{--<li class="nav-item nav-dropdown {{--}}
+                    {{--active_class(Route::is('admin/auth*'), 'open')--}}
+                {{--}}">--}}
+                {{--<a class="nav-link nav-dropdown-toggle {{--}}
+                        {{--active_class(Route::is('admin/auth*'))--}}
+                    {{--}}" href="#">--}}
+                    {{--<i class="nav-icon far fa-user"></i>--}}
+                    {{--@lang('labels.menu.member_management')--}}
+                {{--</a>--}}
+
+                {{--<ul class="nav-dropdown-items">--}}
+                    {{--<li class="nav-item">--}}
+                        {{--<a class="nav-link nav-child {{--}}
+                    {{--active_class(\Request::is('admin/member*'))--}}
+                {{--}}" href="{{ route('member.index', 'female') }}">--}}
+                            {{--<i class="nav-icon cil-wc"></i>--}}
+                            {{--@lang('labels.menu.user_management')--}}
+                        {{--</a>--}}
+                    {{--</li>--}}
+                    {{--@can('balances_management')--}}
+                        {{--<li class="nav-item">--}}
+                            {{--<a class="nav-link nav-child {{--}}
+                    {{--active_class(Route::is('admin/balances'))--}}
+                {{--}}" href="{{ route('member.balances') }}">--}}
+                                {{--<i class="nav-icon cib-auth0"></i>--}}
+                                {{--@lang('labels.menu.balances')--}}
+                            {{--</a>--}}
+                        {{--</li>--}}
+                    {{--@endcan--}}
+
+                    {{--@can('offer_management')--}}
+                        {{--<li class="nav-item">--}}
+                            {{--<a class="nav-link nav-child {{--}}
+                                {{--active_class(Route::is('admin/auth/user*'))--}}
+                            {{--}}" href="{{ route('offers.index') }}">--}}
+                                {{--<i class="nav-icon cib-todoist"></i>--}}
+                                {{--@lang('labels.menu.offer')--}}
+                            {{--</a>--}}
+                        {{--</li>--}}
+                    {{--@endcan--}}
+                {{--</ul>--}}
+            {{--</li>--}}
+
+            {{--<li class="divider"></li>--}}
+            {{--<li class="nav-item">--}}
+                {{--<a class="nav-link {{--}}
+                    {{--active_class(\Request::is('admin/contact*'))--}}
+                {{--}}" href="{{ route('contact.index') }}">--}}
+                    {{--<i class="nav-icon cil-envelope-closed"></i>--}}
+                    {{--@lang('labels.menu.contact')--}}
+                {{--</a>--}}
+            {{--</li>--}}
 
 
             <li class="nav-item">
@@ -146,60 +151,31 @@
             </li>
 
 
-            <li class="divider"></li>
-            <li class="nav-item nav-dropdown {{
-                    active_class(Route::is('admin/auth*'), 'open')
-                }}">
-                <a class="nav-link nav-dropdown-toggle {{
-                        active_class(Route::is('admin/auth*'))
-                    }}" href="#">
-                    <i class="nav-icon cil-credit-card"></i>
-                    @lang('labels.menu.content')
-                </a>
-                <ul class="nav-dropdown-items">
-                    <li class="nav-item">
-                        <a class="nav-link nav-child {{
-                                active_class(Route::is('admin/auth/user*'))
-                            }}" href="{{ route('banners.index') }}">
-                            <i class="nav-icon cil-inbox"></i>
-                            @lang('labels.menu.banner')
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link nav-child {{
-                                active_class(Route::is('admin/auth*'))
-                            }}" href="{{ route('news.index') }}">
-                            <i class="nav-icon cil-bell"></i>
-                            @lang('labels.menu.notification_management')
-                        </a>
-                    </li>
-                </ul>
-            </li>
 
-            <li class="divider"></li>
+            {{--<li class="divider"></li>--}}
 
-            @can('setting_management')
-                <li class="nav-item nav-dropdown {{
-                    active_class(Route::is('admin/auth*'), 'open')
-                }}">
-                    <a class="nav-link nav-dropdown-toggle {{
-                        active_class(Route::is('admin/auth*'))
-                    }}" href="#">
-                        <i class="nav-icon cil-settings"></i>
-                        @lang('labels.menu.setting_management')
-                    </a>
-                    <ul class="nav-dropdown-items">
-                        <li class="nav-item">
-                            <a class="nav-link nav-child {{
-                                active_class(Route::is('admin/auth/user*'))
-                            }}" href="{{ route('banks.edit') }}">
-                                <i class="nav-icon cil-settings" aria-hidden="true"></i>
-                                @lang('labels.menu.bank_management')
-                            </a>
-                        </li>
-                    </ul>
-                </li>
-            @endcan
+            {{--@can('setting_management')--}}
+                {{--<li class="nav-item nav-dropdown {{--}}
+                    {{--active_class(Route::is('admin/auth*'), 'open')--}}
+                {{--}}">--}}
+                    {{--<a class="nav-link nav-dropdown-toggle {{--}}
+                        {{--active_class(Route::is('admin/auth*'))--}}
+                    {{--}}" href="#">--}}
+                        {{--<i class="nav-icon cil-settings"></i>--}}
+                        {{--@lang('labels.menu.setting_management')--}}
+                    {{--</a>--}}
+                    {{--<ul class="nav-dropdown-items">--}}
+                        {{--<li class="nav-item">--}}
+                            {{--<a class="nav-link nav-child {{--}}
+                                {{--active_class(Route::is('admin/auth/user*'))--}}
+                            {{--}}" href="{{ route('banks.edit') }}">--}}
+                                {{--<i class="nav-icon cil-settings" aria-hidden="true"></i>--}}
+                                {{--@lang('labels.menu.bank_management')--}}
+                            {{--</a>--}}
+                        {{--</li>--}}
+                    {{--</ul>--}}
+                {{--</li>--}}
+            {{--@endcan--}}
 
             @can('account_management')
                 <li class="nav-item {{
